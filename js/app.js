b1ae8457e4c935f92f917465b72cb1ac312b6242
AfianAvro <wahedemon09@gmail.com>
2021-09-13 03:57:48 +0600

    fix

--- js/app.js
+++ js/app.js
@@ -286,7 +286,7 @@ const showProducts = (products) => {
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h5><i class="fas fa-star text-warning"></i> ${product.rating.rate}</h5>
-      <h5> Total rate: <i class="fas fa-user-astronaut text-danger"></i> ${product.rating.count}</h5>
+      <h5> Peoples rating: <i class="fas fa-user-astronaut text-danger"></i> ${product.rating.count}</h5>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-primary">add to cart</button>
       <button id="details-btn" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="showDetails('${product.description}')" class="btn btn-danger">Details</button></div>
