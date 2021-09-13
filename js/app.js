b576bfe7ddb08e1238847e537daaba3bdb0c813f
AfianAvro <wahedemon09@gmail.com>
2021-09-13 10:43:28 +0600

    value chng

--- js/app.js
+++ js/app.js
@@ -354,6 +354,6 @@ const updateTotal = () => {
     getInputValue("price") +
     getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText =grandTotal;
+  document.getElementById("total").innerText =grandTotal.toFixed(2);
 };
 loadProducts();
